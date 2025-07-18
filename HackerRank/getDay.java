3b279cf3541fe1e997e35881302ada2ffe8605aa
Shabbir <shabbirhassain42@gmail.com>
2025-07-18 07:28:20 -0700

    problem solve on getDay class

--- HackerRank/getDay.java
+++ HackerRank/getDay.java
@@ -3,7 +3,7 @@ import java.util.Calendar;
 public class getDay {
      public static String findDay(int month, int day, int year) {
         Calendar c = Calendar.getInstance();
-        c.set(month,day, year);
+        c.set(year,month-1,day);
         int day_of_week = c.get(Calendar.DAY_OF_WEEK);
         String[] Days = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
         
