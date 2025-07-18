9e3f8e9e16f80da0ac1231239485e2cc57e78029
Shabbir <shabbirhassain42@gmail.com>
2025-07-18 07:22:19 -0700

    get day method

---
+++ HackerRank/getDay.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+
+public class getDay {
+     public static String findDay(int month, int day, int year) {
+        Calendar c = Calendar.getInstance();
+        c.set(month,day, year);
+        int day_of_week = c.get(Calendar.DAY_OF_WEEK);
+        String[] Days = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+        
+        return Days[day_of_week-1];
+    }
+    public static void main(String[] args) {
+        String day = findDay(07, 18, 2025);
+        System.out.println(day);
+    }
+}
